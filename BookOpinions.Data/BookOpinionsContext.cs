01e0c5293afea411bd13db664d9783d1a585b1ce
Doncho Patronski <donstz@yahoo.com>
2017-04-10 23:04:45 +0300

    neshto

---
+++ BookOpinions.Data/BookOpinionsContext.cs
@@ -0,0 +1,31 @@
+namespace BookOpinions.Data
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class BookOpinionsContext : DbContext
+    {
+        // Your context has been configured to use a 'BookOpinionsContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'BookOpinions.Data.BookOpinionsContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'BookOpinionsContext' 
+        // connection string in the application configuration file.
+        public BookOpinionsContext()
+            : base("name=BookOpinionsContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
