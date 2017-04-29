b9ca3c5b51eaa3b5d5f453edc65cf934abb89bc7
Doncho Patronski <donstz@yahoo.com>
2017-04-29 21:54:20 +0300

    tips

---
+++ BookOpinions/Handlers/UserInfoHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookOpinions.Handlers
+{
+    public class UserInfoHandler : IHttpHandler
+    {
+        public bool IsReusable => throw new NotImplementedException();
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string userAgent = context.Request.UserAgent;
+            string userIp = context.Request.UserHostAddress;
+            context.Response.Write($"<p>User Agent: {userAgent} </br> User IP address: {userIp}</p>");
+        }
+    }
+}
